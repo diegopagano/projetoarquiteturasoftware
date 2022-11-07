796771e85a5e4aa0643bb8cf8e3e0e119a57fa04
ap609 <ap609@LAPTOP-HTOOE3E3>
2022-11-07 20:30:11 -0300

    ajuste cancelar agenda

--- src/main/java/br/com/desafio/springbootapi/controller/CancelarAtividadeRest.java
+++ src/main/java/br/com/desafio/springbootapi/controller/CancelarAtividadeRest.java
@@ -1,14 +1,22 @@
 package br.com.desafio.springbootapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.desafio.springbootapi.repository.AgendaRepositorio;
+
 @RestController
 public class CancelarAtividadeRest {
-	@RequestMapping(value = "/cancelarAtividade", method = RequestMethod.GET, produces = "application/json") // PATH > OQ VAI FAZER
-	public String cancelarAtividade() { // retorna uma lista de todas todos
-
-		return "aqui vai o metodo cancelar atividade aluno";
+	
+	@Autowired
+	private AgendaRepositorio agendaRepositorio;
+	
+	@RequestMapping(value = "/cancelarAtividade/{id}", method = RequestMethod.GET, produces = "application/json") // PATH > OQ VAI FAZER
+	public String cancelarAtividade(@PathVariable(value = "id") Integer id) { // retorna uma lista de todas todos
+		agendaRepositorio.deleteById(id);
+		return "atividade cancelada";
 	}
 }
